74695e592c72a958291c929c84663de46375d71a
Lukas Danin <lukas.danin@gmail.com>
2020-04-09 03:30:14 -0300

    Adds a compiled front-end build

---
+++ build/precache-manifest.1615f86de974224da939d74d4b831f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411c75655ce687a238d70bad0d1356a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84db7e48b4f2ce6e8b77",
+    "url": "/static/css/main.954ec30f.chunk.css"
+  },
+  {
+    "revision": "606f16c4b90bf648ea22",
+    "url": "/static/js/2.250f4d3a.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/static/js/2.250f4d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84db7e48b4f2ce6e8b77",
+    "url": "/static/js/main.216e9ecf.chunk.js"
+  },
+  {
+    "revision": "a1c520ec29793b1b9109",
+    "url": "/static/js/runtime-main.e25f9c23.js"
+  }
+]);
